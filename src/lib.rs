5375d7d9eebb1e7ed7eaadf4dc970cfd41cfde91
Rail Khusnutdinov <mail@rail-ka.ru>
2021-10-04 18:29:39 +0300

    del PrimitiveFromEnum

--- src/lib.rs
+++ src/lib.rs
@@ -22,27 +22,3 @@ type FxBuildHasher = BuildHasherDefault<FxHasher>;
 
 #[cfg(all(any(feature = "hashmap", feature = "std"), feature = "map"))]
 type FxHashMap<K, V> = HashMap<K, V, FxBuildHasher>;
-
-/**
-Need for complex Enums, which includes other data:
-```
-enum Complex {
-    A(String),
-    B(u32),
-    C
-}
-enum Primitive {
-    A,
-    B,
-    C,
-}
-```
-PrimitiveEnum should be equivalent for Complex, but without variants inner data
-*/
-pub trait PrimitiveFromEnum {
-    type PrimitiveEnum;
-
-    fn get_primitive_enum(&self) -> Self::PrimitiveEnum;
-
-    fn primitive_name() -> &'static str;
-}
